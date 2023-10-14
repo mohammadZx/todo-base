bdabed1350738a99a55c13df9f60047e35cb59b5
mohammad <40648884+mohammadZx@users.noreply.github.com>
2023-10-14 15:36:02 +0330

    Add files via upload

---
+++ action.php
@@ -0,0 +1,11 @@
+<?php
+    // write your php here!
+    // dbuser= root
+    // dbpass = empty
+    // dbname = todo
+    // todotable = todos
+    // todo table columns: id, todo, date, status
+    // phpmyadminaddress = http:///localhost/phpmyadmin
+
+?>
+
